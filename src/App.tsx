18723e5daa3bcdb730df506f0b5e646198eb4c13
Avishek Adhikary <avishekadhikary42@gmail.com>
2024-10-22 21:39:14 +0530

    react navigation installation config

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import Home from './Screens/Home'
 import { ContextProvider } from './Context'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
     <GestureHandlerRootView>
-      <ContextProvider>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name='Home' component={Home} />
-        </Stack.Navigator>
-      </ContextProvider>
+      <NavigationContainer>
+        <ContextProvider>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home} />
+          </Stack.Navigator>
+        </ContextProvider>
+      </NavigationContainer>
     </GestureHandlerRootView>
   )
 }
